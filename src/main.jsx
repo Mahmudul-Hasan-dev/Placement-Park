e1e601fa7639964b1b517f7c3e1e5f549fc2b787
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2024-04-22 22:54:39 +0600

    Routes

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,42 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Layout from './components/Layout/Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import JobDetails from './components/JobDetails/JobDetails.jsx';
+import AppliedJob from './components/AppliedJob/AppliedJob.jsx';
+import Statistics from './components/Statistics/Statistics.jsx';
+import Blog from './components/Blog/Blog.jsx';
+import ErrorPage from './components/404/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
+    errorElement: <ErrorPage />,
     children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch('jobs.json')
+      },
+      {
+        path: "/job/:id",
+        element: <JobDetails></JobDetails>,
+        loader: ({ params }) => fetch(`/${params.id}`)
 
+      },
+      {
+        path: "/applied",
+        element: <AppliedJob></AppliedJob>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      },
     ]
 
   }
