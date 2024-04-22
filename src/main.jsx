19b5bf4a455f6b314a617f0032374f8ae0ad4a35
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2024-04-22 21:35:47 +0600

    Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Layout from './components/Layout/Layout.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+
+    ]
+
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
